4a7d845cf0fb3d7ffddee92a4438cffbd2d84442
Bhargav-Talluri <103348556+Bhargav-Talluri@users.noreply.github.com>
2022-04-19 20:53:00 +0530

    Update 19th,April.java

--- 19th,April.java
+++ 19th,April.java
@@ -167,3 +167,63 @@ public class BoxDemo {
 		System.out.println("volume is "+mybox2.volume());
 	}
 }
+
+package OopsConcepts;
+
+public class stack {
+	int stck[] = new int[10];
+	int tos;
+
+	public stack() {
+		tos =-1;
+	}
+	
+	void push(int item) {
+		if(tos==9) {
+			System.out.println("Stack is full");
+		}else {
+			stck[++tos] = item;
+		}
+		}
+		
+	int pop(){
+		if(tos<0) {
+			System.out.println("Stack underflow");
+			return 0;
+		}else {
+			return stck[tos--];
+		}
+	}
+		
+	}
+
+package OopsConcepts;
+
+public class TestStack {
+
+	public static void main(String[] args) {
+		stack myStack1 = new stack();
+		stack myStack2 = new stack();
+		
+		for(int i = 0 ; i<10; i++) {
+			myStack1.push(i);
+		}
+		
+		for(int i = 10;i<20;i++) {
+			myStack2.push(i);
+		}
+		
+		System.out.println("stack in mystack1:");
+		for(int i = 0;i<10;i++) {
+			System.out.println(myStack1.pop());
+		}
+		
+		System.out.println("stack in mystack2:");
+		for(int i = 0;i<10;i++) {
+			System.out.println(myStack2.pop());
+		}
+
+	}
+
+}
+
